0c9ac203af663c72c8bdb9ffc90bb7f30aa8619c
Kurt Spindler <kurt@uber.com>
2014-12-19 01:57:50 -0800

    lots of updates to readme & doc. new cli flags

--- README.md
+++ README.md
@@ -1,3 +1,134 @@
 # Puffin
 
-Python replacement for awk
+Puffin is a Python replacement for awk. You already know Python. Turn this:
+
+    $ wc -l *.txt  | awk '{s+=$1} END {print $s}'
+    468
+
+into this:
+
+    $ wc -l *.txt  | puf 'sum(cols[0])'
+    468
+
+Puffin is under 200 lines of Python and will stay extremely lean.
+PRs, issues (especially use cases that Puffin doesn't currently address) welcome.
+
+## Install
+
+    pip install puffin
+
+## Quickstart
+
+#### Puffin evaluates arbitrary python code
+
+    $ puf 27/2.5
+    10.8
+
+    $ puf 2**20
+    1048576
+
+    $ puf 'range(3)'
+    0
+    1
+    2
+
+#### Puffin understands the input
+
+    $ wc -l *.txt  | puf 'sum(cols[0])'
+    468
+
+
+#### Puffin can do complex filters in a single pass
+
+    # gives pids of all processes owned by kurt
+    $ ps aux | puf -l 'row[1] if row[0] == "kurt" else None'
+    5231
+    155
+    ...
+
+## Description
+
+#### Basics
+
+Puffin acts by creating a namespace of 3 variables (`lines`, `rows`, `cols`), and then evaluating
+the given python command. It then attempts to intelligently print this result.
+
+lines
+    an array of each newline-stripped line of stream
+rows
+    each line of the stream, separated into components
+cols
+    each column of the stream
+
+Some examples of these in action
+
+    # Turn a list into comma-separated values
+    $ cat invalid.txt | puf '",".join(lines)'
+    3021,4439,9544,3985,1262
+
+    # Check the validity of a csv
+    $ cat cities.csv | puf -s, '[len(r) for r in rows]'
+    13
+    14
+    13
+    13
+
+    # Get all pids
+    $ ps aux | puf 'cols[1]'
+    PID
+    5231
+    155
+    ...
+
+    # Remove that PID header
+    $ ps aux | puf 'cols[1][1:]'
+    5231
+    155
+    ...
+
+#### Line operation
+
+With the `-l` option, Puffin evaluates the command on each line individually.
+Lines that evaluate to None are skipped, allowing complex querying.
+The provided namespace is just `line` and `row`.
+
+    # Silly filter for all PIDs that
+    $ ps aux | puf -hl 'row[1] if row[0] == "kurt" and row[1] % 10 == 0 else None'
+
+#### Import statements
+
+Puffin will attempt to import anything that is unreferenced.
+
+    $ puf 'uuid.uuid4()'
+    f079dbbe-6bbc-430a-a3d1-e0f53a0ef719
+
+Specifically, Puffin will catch `NameError`s and attempt an import. If that fails with ImportError, it re-raises.
+
+## Options
+
+    -l, --line
+
+Puffin normally operates on the entire stream at a single time. This option changes that behavior to perform the operation per line.
+Among other things, this allows for complex filtering in a single pass. This also changes the given namespace to `line` and `row`.
+
+  -s SEPARATOR, --separator SEPARATOR
+
+Use this option to split lines into rows and columns on something other than whitespace (comma is a common alternative)
+
+      -h, --skip-header
+
+Some streams (`ps`, csv files) have headers. This option skips the first line of the file so you don't have to worry about
+handling it correctly.
+
+      -r, --raw
+
+Puffin tries to intelligently display your results. Use this option to bypass those attempts and display the raw result.
+
+      -i INITIAL, --initial INITIAL
+
+Puffin will execute this command before anything else, in order to set up accumulation variables, do import renames, or other setup.
+
+      -f FILE, --file FILE
+
+Puffin can instead execute a regular python file with the namespace of `lines`, `rows`, `cols`. Puffin executes this file
+as normal Python, without intelligent printing of any results, etc. This option is incompatible with -l and -r.
--- puffin/__init__.py
+++ puffin/__init__.py
@@ -1,14 +1,17 @@
+from __future__ import division, print_function
 import sys
 import argparse
-
-# additional namespaces for execution
-import random
-import os
-import re
 import collections
-import math
+import re
+
 
 def interp(i):
+    """
+    Attempt to coerce i to an int or float
+
+    :param i:
+    :return:
+    """
     try:
         return int(i)
     except ValueError:
@@ -20,12 +23,36 @@ def interp(i):
     return i
 
 
-def parse(stream, sep=None):
+def parse_lines(stream, separator=None):
+    """
+    Takes each line of a stream, creating a generator that yields
+    tuples of line, row - where row is the line split by separator
+    (or by whitespace if separator is None.
+
+    :param stream:
+    :param separator: (optional)
+    :return: generator
+    """
+    for line in stream:
+        line = line.rstrip('\n')
+        row = [interp(i) for i in line.split(separator)]
+        yield line, row
+
+
+def parse_buffer(stream, separator=None):
+    """
+    Returns a dictionary of the lines of a stream, an array of rows of the
+     stream (split by separator), and an array of the columns of the stream
+     (also split by separator)
+
+    :param stream:
+    :param separator:
+    :return: dict
+    """
     rows = []
     lines = []
-    for line in stream:
-        lines.append(line.rstrip('\n'))
-        row = [interp(i) for i in line.split(sep)]
+    for line, row in parse_lines(stream, separator):
+        lines.append(line)
         rows.append(row)
     cols = zip(*rows)
     return {
@@ -36,38 +63,121 @@ def parse(stream, sep=None):
 
 
 def display(result):
-    if isinstance(result, basestring):
-        return result
-    if isinstance(result, collections.Mapping):
-        return '\n'.join('%s=%s' % (k, v) for k, v in result.iteritems())
-    if isinstance(result, collections.Iterable):
-        return '\n'.join(str(x) for x in result)
+    """
+    Intelligently print the result (or pass if result is None).
+
+    :param result:
+    :return: None
+    """
+    if result is None:
+        pass
+    elif isinstance(result, basestring):
+        print(result)
+    elif isinstance(result, collections.Mapping):
+        print('\n'.join('%s=%s' % (k, v) for k, v in result.iteritems()))
+    elif isinstance(result, collections.Iterable):
+        print('\n'.join(str(x) for x in result))
     else:
-        return str(result)
+        print(str(result))
+
+
+def retry_eval(command, glob, local):
+    """
+    Continue to attempt to execute the given command, importing objects which
+    cause a NameError in the command
+
+    :param command: command for eval
+    :param glob: globals dict for eval
+    :param local: locals dict for eval
+    :return: command result
+    """
+    try:
+        return eval(command, glob, local)
+    except NameError as e:
+        match = re.match("name '(.*)' is not defined", e.message)
+        if not match:
+            raise e
+        try:
+            exec ('import %s' % (match.group(1), )) in glob
+        except ImportError:
+            raise e
+        return retry_eval(command, glob, local)
+
+
+def execute(local, glob, command=None, file=None, raw=False):
+    """
+    Execute either command or file using the passed dictionaries.
+    Intelligently print the result (unless raw=True is passed)
+
+    :param local: locals dict for exec
+    :param glob: globals dict for exec
+    :param command: command for eval
+    :param file: file for exec
+    :param raw: bool to print the result directly
+    :return: None
+    """
+    if file:
+        execfile(file, glob, local)
+    else:
+        result = retry_eval(command, glob, local)
+        if raw:
+            print(result)
+        else:
+            display(result)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--separator', help='Separator between lines', default=None)
-    parser.add_argument('-r', '--raw', help='Do not coerce row results', action='store_true', default=False)
-    parser.add_argument('-f', '--file', help='File to execute (instead of passed command.)', default=None)
+    parser = argparse.ArgumentParser(add_help=False)
+    # for, line, puf -
+    parser.add_argument('-l', '--line',
+                    help='Execute the command for each line of input.',
+                    action='store_true', default=False)
+    parser.add_argument('-s', '--separator', help='Custom column separator.', default=None)
+    parser.add_argument('-h', '--skip-header',
+                        help='Skip the first line of the stream.',
+                        action='store_true', default=False)
+    parser.add_argument('-r', '--raw',
+                        help='Print the raw result of the command. '
+                             '(No smart display.)',
+                        action='store_true', default=False)
+    parser.add_argument('-i', '--initial',
+                        help='Statement to execute before the command '
+                             '(e.g. set up accumulation variables).',
+                        default=None)
+    parser.add_argument('-f', '--file',
+                        help='Execute the file (instead of a '
+                             'command). Incompatible with -l and -r.',
+                        default=None)
+    parser.add_argument('--help', action='help', help='Display this help message.')
+    parser.add_argument('--version', action='store_true', help='Display the version.')
     parser.add_argument('command', nargs='?')
 
     args = parser.parse_args()
 
-    stream = sys.stdin
-    if not stream.isatty():
-        parsed_locals = parse(sys.stdin, args.separator)
-    else:
-        parsed_locals = {}
+    if args.version:
+        import pkg_resources
+        print(pkg_resources.get_distribution('puffin').version)
+        return
+    if not args.command or args.file:
+        return parser.print_help()
 
-    if args.file:
-        execfile(args.file, globals(), parsed_locals)
-    elif args.command:
-        result = eval(args.command, globals(), parsed_locals)
-        if args.raw:
-            print result
-        else:
-            print display(result)
+    stream = sys.stdin
+
+    if args.initial:
+        exec args.initial in globals()
+
+    if stream.isatty():
+        execute({}, globals(), args.command, args.file, args.raw)
     else:
-        parser.print_help()
+        if args.skip_header:
+            stream.readline()  # skip, so no action necessary
+        if args.line:
+            for line, row in parse_lines(stream, args.separator):
+                local = {
+                    'line': line,
+                    'row': row,
+                }
+                execute(local, globals(), args.command, args.file, args.raw)
+        else:
+            local = parse_buffer(stream, args.separator)
+            execute(local, globals(), args.command, args.file, args.raw)
