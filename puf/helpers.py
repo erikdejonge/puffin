c7b7cae1e151cf82155e08cb73817c0202628ba9
Kurt Spindler <kurt@uber.com>
2015-01-26 08:23:10 -0800

    add flags to puffin regex matchers

--- puf/helpers.py
+++ puf/helpers.py
@@ -2,12 +2,12 @@ from __future__ import division, unicode_literals, absolute_import
 import re
 
 
-def match(regex, string):
-    return search('^' + regex, string)
+def match(regex, string, flags=0):
+    return search('^' + regex, string, flags)
 
 
-def search(regex, string):
-    match = re.search(regex, string)
+def search(regex, string, flags=0):
+    match = re.search(regex, string, flags)
     if match:
         if not match.groups():
             return string
