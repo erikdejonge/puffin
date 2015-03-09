e86197daa46879ca8ec1f8570fa7c2072be04be6
Erik de Jonge <erik@a8.nl>
2015-03-09 18:50:54 +0100

    test Mon Mar  9 18:50:54 CET 2015

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ python3 tests.py
 git commit -am "test `date`"
 git push
 
-docker build -t puftestp27 .
+docker build --no-cache=true -t puftestp27 .
 docker run -it --rm puftestp27 python /puffin/tests.py
